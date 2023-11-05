bf8af0aa1b777bf48bdab353988af30dc4303815
Sheejan Tripathi <sheejantripathi101@gmail.com>
2023-11-05 20:33:58 +0100

    file encryption and upload

--- modules/contractInstances/instances.controller.js
+++ modules/contractInstances/instances.controller.js
@@ -1,14 +1,16 @@
 const { Web3 } = require('web3');
+// const web3 = new Web3("https://rpc2.sepolia.org");
 const providers = new Web3.providers.HttpProvider('http://127.00.1:7545');
-let web3 = new Web3(providers);
 require('dotenv').config();
-const { Readable } = require('stream');
+let web3 = new Web3(providers);
 
+const { Readable } = require('stream');
+const crypto = require('crypto');
 
 const { PINATA_API_KEY, PINATA_SECRET_API_KEY} = process.env;
-console.log(PINATA_API_KEY, PINATA_SECRET_API_KEY, 'yo i am here man')
 const pinataSDK  = require('@pinata/sdk');
 const PinataClient = new pinataSDK(PINATA_API_KEY, PINATA_SECRET_API_KEY);
+const Controller = require('../users/user.controller');
 // const {ethers} = require('ethers');
 // const INFURA_API_KEY = 'd65a7226079f4b9d9da2e1f694dfcda8'
 // const provider = new ethers.BrowserProvider(window.ethereum);
@@ -16,9 +18,14 @@ const PinataClient = new pinataSDK(PINATA_API_KEY, PINATA_SECRET_API_KEY);
 const policyManager = require('../../build/contracts/PolicyManager.json');
 const childContract = require('../../build/contracts/ChildContract.json');
 
+// const {encryptFile} = require('./ipfs-encrypt');
+
 // const pinatasdk = require('api')('@pinata-cloud/v1.0#12ai2blmsggcsb');
 
 async function createPolicyContractInstance() {
+    // const web3 = new Web3(
+    //     new Web3(`${process.env.INFURA_API_KEY}`)
+    //   );
     const networkId = await web3.eth.net.getId();
     const deployedNetwork = policyManager.networks[networkId];
     const instance = new web3.eth.Contract(
@@ -28,6 +35,27 @@ async function createPolicyContractInstance() {
     return instance;
 }
 
+async function createPolicyContractInstanceSepolia() {
+     // Configuring the connection to an Ethereum node
+  const network = process.env.ETHEREUM_NETWORK;
+  const web3 = new Web3(
+    new Web3(`${process.env.INFURA_API_KEY}`)
+  );
+  // Creating a signing account from a private key
+  const signer = web3.eth.accounts.privateKeyToAccount(
+    "0x" + process.env.SIGNER_PRIVATE_KEY
+  );
+  web3.eth.accounts.wallet.add(signer);
+  // Creating a Contract instance
+  const contract = new web3.eth.Contract(
+    policyManager.abi,
+    // Replace this with the address of your deployed contract
+    process.env.DEMO_CONTRACT,
+  );
+
+  return contract;
+}
+
 async function createChildContractInstance(contractAddress) {
     const networkId = await web3.eth.net.getId();
     const instance = new web3.eth.Contract(
@@ -35,6 +63,108 @@ async function createChildContractInstance(contractAddress) {
         contractAddress,
     );
     return instance;
+}
+
+async function encryptFile(file) {
+    try {
+      // Read the file content
+    //   const fileContent = fs.readFileSync(filePath);
+  
+      // Generate an encryption key and IV
+      const key = crypto.randomBytes(32); // 256-bit key
+      const iv = crypto.randomBytes(16); // 128-bit IV
+  
+      // Create a cipher using AES-256-CBC algorithm
+      const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  
+      // Encrypt the file content
+      let encrypted = cipher.update(file.data);
+      encrypted = Buffer.concat([encrypted, cipher.final()]);
+  
+      // Save the encrypted content to a new file
+    //   const encryptedFilePath = filePath + '.enc';
+    //   fs.writeFileSync(encryptedFilePath, encrypted);
+
+  
+      return {
+        encrypted,
+        key: key.toString('hex'),
+        iv: iv.toString('hex')
+      };
+    } catch (error) {
+      console.error('Encryption failed:', error);
+      return null;
+    }
+  }
+
+async function decryptFile(encryptedBuffer, key, iv) {
+    try {
+        const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key, 'hex'), Buffer.from(iv, 'hex'));
+        let decrypted = decipher.update(encryptedBuffer);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted;
+    } catch (error) {
+        console.error('Decryption failed:', error);
+        return null;
+    }
+}
+
+  
+
+async function encryptStreamAndUploadToIPFS(filesToUpload, asset_owner) {
+
+    let filesUploaded = [];
+    const publicKey = JSON.stringify();
+
+    for (const file of filesToUpload) {
+        let encryptedFile = await encryptFile(file);
+        console.log(encryptedFile, 'encryptedFile')
+        let options = {
+            pinataMetadata: {
+                name: file.name,
+                keyvalues: {
+                    owner: asset_owner,
+                    fileSize: file.size
+                }
+            },
+            pinataOptions: {
+                cidVersion: 0
+            }
+        };
+         
+        // Assuming fileData is your file object
+        const fileBuffer = encryptedFile.encrypted;      
+        // Create a Readable stream from the file buffer
+        const readableStream = new Readable();
+        readableStream._read = () => {}; // Necessary for Readable stream
+
+        readableStream.push(fileBuffer);
+        readableStream.push(null);
+        // let result = await encryptStreamAndUploadToIPFS(readableStream, options);
+        let result = await PinataClient.pinFileToIPFS(readableStream, options)
+        filesUploaded.push({...result, fileName:file.name, key: encryptedFile.key, iv: encryptedFile.iv});
+    }
+
+    return filesUploaded;
+  // Usage example
+  let data = Buffer.alloc(0);
+
+  // Read the stream data into a buffer
+  readStream.on('data', chunk => {
+    data = Buffer.concat([data, chunk]);
+  });
+
+  readStream.on('end', async () => {
+    try {
+      const encryptedData = await encrypt(data);
+      console.log(encryptedData, 'encryptedData')
+      const result = await PinataClient.pinFileToIPFS(encryptedData, options)
+      console.log(result, 'result')
+      return result; 
+    } catch (error) {
+      console.error('Encryption or pinning failed:', error);
+    }
+  });
 }
 
 
@@ -51,7 +181,8 @@ async function getFilesAssociatedWithGroups(groupAddress) {
 }
 
 async function getFilesAssociatedWithUser(userAddress) {
-    console.log(userAddress, 'userAddress')
+    // console.log(userAddress, 'userAddress')
+    // const user = await Controller.findUser({publicAddress: asset_owner});
     let metadataFilter = {
         keyvalues: {
             owner: {
@@ -71,7 +202,8 @@ async function getFilesAssociatedWithUser(userAddress) {
 
 async function uploadFileToIPFS(filesToUpload, asset_owner) {
     let filesUploaded = [];
-    for (const file of filesToUpload) {
+
+    for (const file of filesToUpload[0]) {
         // Process each file here
         let options = {
             pinataMetadata: {
@@ -95,6 +227,7 @@ async function uploadFileToIPFS(filesToUpload, asset_owner) {
         readableStream.push(fileBuffer);
         readableStream.push(null);
         let result = await PinataClient.pinFileToIPFS(readableStream, options)
+        result = {...result, fileName:file.name}
         filesUploaded.push(result);
       }
    
@@ -113,11 +246,16 @@ async function updateIPFSFileMetadata(ipfsPinHash, contractAddress, contractName
 
 }
 
+
+
 module.exports = {
     createPolicyContractInstance, 
     createChildContractInstance, 
     getFilesAssociatedWithGroups, 
     uploadFileToIPFS,
     getFilesAssociatedWithUser,
-    updateIPFSFileMetadata
+    updateIPFSFileMetadata,
+    encryptStreamAndUploadToIPFS,
+    createPolicyContractInstanceSepolia,
+    decryptFile
 };
--- modules/users/user.routes.api.js
+++ modules/users/user.routes.api.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const Controller = require("./user.controller");
 const authenticateToken = require("../../helpers/utils/jwt-middleware");
 const instanceController = require('../contractInstances/instances.controller');
+const crypto = require('crypto');
+const NodeRSA = require('node-rsa');
+
 
 
 /** GET /api/users */
@@ -32,8 +35,6 @@ router.get('/', async (req, res, next) => {
   });
  
 router.get('/:userId', authenticateToken, async (req, res, next) => {
-    // console.log(req.file, req.files)
-    console.log(req.user);
 
     if (req.user.id !== req.params.userId) {
         return res
@@ -43,7 +44,6 @@ router.get('/:userId', authenticateToken, async (req, res, next) => {
 
     await Controller.getById(req.params.userId)
     .then((user) => {
-        console.log(user, 'user')
         res.json(user)
     })
     .catch(err=>next(err))
@@ -58,16 +58,112 @@ router.post('/', async (req, res, next) => {
 
 /** POST /api/users/asset-upload */
 router.post('/asset-upload',authenticateToken, async (req, res, next) => {
-    if (!req.files) {
-        return res.status(400).send('No files uploaded.');
-      }
-    
-    const asset_owner = req.user.publicAddress
-    const filesToUpload = Object.values(req.files);
-    await instanceController.uploadFileToIPFS(filesToUpload, asset_owner)
-    .then((uploadedFiles) => res.json(uploadedFiles))
-    .catch((err)=> console.log(err));
+  if (!req.files) {
+    return res.status(400).send('No files uploaded.');
+  }
+  
+  const asset_owner = req.user.publicAddress;
+  const filesToUpload = Object.values(req.files);
+  
+  console.log(filesToUpload, 'filesToUpload')
+  return
+  try {
+    const uploadedFiles = await instanceController.encryptStreamAndUploadToIPFS(filesToUpload, asset_owner);
+  
+    for (file of uploadedFiles) {
+      const user = await Controller.findUser({ publicAddress: asset_owner });
+  
+      const encryptedSymmetricKey = crypto.publicEncrypt(
+        {
+          key: user.publicKey,
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+          oaepHash: "sha256",
+        },
+        Buffer.from(file.key)
+      );
+
+      const encryptedSymmetricIV = crypto.publicEncrypt(
+        {
+          key: user.publicKey,
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+          oaepHash: 'sha256',
+        },
+        Buffer.from(file.iv)
+      );
+ 
+      console.log(file, 'file')
+      const factoryContractInstance = await instanceController.createPolicyContractInstance();
+      // console.log(encryptedSymmetricKey, 'encryptedSymmetricKey')
+      // console.log(encryptedSymmetricIV, 'encryptedSymmetricIV')
+
+      // return
+      // Convert hexadecimal strings to bytes
+      const encryptedSymmetricKeyHex = '0x' + encryptedSymmetricKey.toString('hex');
+      const encryptedSymmetricIVHex = '0x' + encryptedSymmetricIV.toString('hex');
+      const fileContractResult = await factoryContractInstance.methods.createFileContract(file.fileName, file.IpfsHash, 
+        encryptedSymmetricKeyHex, encryptedSymmetricIVHex).send({ from: asset_owner, gas: 3000000 });
+  
+      const updatedUser = await Controller.associateFilesToOwner({
+        eoaAddress: asset_owner,
+        contractAddress: fileContractResult.to,
+        fileName: file.name
+      });
+      console.log(updatedUser, 'updatedUser')
+      res.json(updatedUser);
+    }
+  } catch (error) {
+    // Handle any caught errors
+    console.error('Error occurred:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+  
+
+    // const decryptedsymmetricKey = crypto.privateDecrypt(
+      //   {
+      //     key: user.privateKey,
+      //     // In order to decrypt the data, we need to specify the
+      //     // same hashing function and padding scheme that we used to
+      //     // encrypt the data in the previous step
+      //     passphrase: 'top secret',
+      //     padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      //     oaepHash: "sha256",
+      //   },
+      //   encryptedSymmetricKey
+      // )
+      
+      // The decrypted data is of the Buffer type, which we can convert to a
+      // string to reveal the original data
+      // console.log("decrypted data: ", decryptedsymmetricKey.toString())
+    // .then((uploadedFiles) => res.json(uploadedFiles))
+    // .catch((err)=> console.log(err));
   });
+
+/** POST /api/usere/asset-uplaod-test*/
+
+router.post('/asset-upload-test',authenticateToken, async (req, res, next) => {
+  if (!req.files) {
+      return res.status(400).send('No files uploaded.');
+    }
+  
+  const asset_owner = req.user.publicAddress
+  const filesToUpload = Object.values(req.files);
+  const ipfsUploadResults = await instanceController.uploadFileToIPFS(filesToUpload, asset_owner);
+  const factoryContractInstance = await instanceController.createPolicyContractInstance();
+  let updatedUsersList = [];
+  for (file of ipfsUploadResults) {
+    const fileContractResult = await factoryContractInstance.methods.createFileContract(file.fileName, file.IpfsHash).send({ from: asset_owner, gas: 3000000 });
+  
+    const updatedUser = await Controller.associateFilesToOwner({
+      eoaAddress: asset_owner,
+      contractAddress: fileContractResult.to,
+      fileName: file.name
+    });
+    updatedUsersList.push(updatedUser);
+  }
+
+  res.json(updatedUsersList);
+});
+
 
 /** POST /api/users/asset-download */
 router.post('/asset-download',authenticateToken, async (req, res, next) => {
@@ -88,8 +184,8 @@ router.put('/:userId',authenticateToken, async (req, res, next) => {
             .status(401)
             .send({ error: 'You can can only access yourself' });
     }
-    // console.log(req.params.id)
-    await Controller.updateUser(req.params.userId, req.body)
+    
+    await Controller.updateUser(req.user, req.body)
         .then((user) => {
             if (!user) {
                 console.log('Such user does not exist')
